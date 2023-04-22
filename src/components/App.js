207880d746638d01b5d192cf101e4009186b63fa
Ashish <ashishkeshri3016@gmail.com>
2023-04-22 20:59:18 +0530

    project setup

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      starting react project 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAr4NNRFV2Xv_0VpmcnXcdZjhdzuRFtn6A",
+    authDomain: "react-hooks-blog-daa63.firebaseapp.com",
+    projectId: "react-hooks-blog-daa63",
+    storageBucket: "react-hooks-blog-daa63.appspot.com",
+    messagingSenderId: "925813774695",
+    appId: "1:925813774695:web:d2718ffae1971fed5b37c2"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
